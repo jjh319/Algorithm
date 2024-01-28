5cfedf47d1b522bb554f20e1a1bf9368d5aa401f
Jang Ji Hun <136777270+jjh319@users.noreply.github.com>
2024-01-28 12:52:53 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,18 @@
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("");
+        String dan = sc.next();
+
+        System.out.println(dan.length());
+
+    } // main
+
+} // end class
