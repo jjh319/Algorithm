825f66c5d87c29bf87b608e558a2e4e7f1ec8cfc
Jang Ji Hun <136777270+jjh319@users.noreply.github.com>
2024-01-29 13:24:08 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		String A;
+		
+		Scanner sc = new Scanner(System.in);	
+		
+		System.out.print("");
+		A = sc.next();
+		
+		int asc = A.charAt(0); 
+		
+		System.out.println(asc);
+		
+	} // main
+
+} // end class
